c229f8b1ae91dada9e80faba7a0e839f03a19069
Hvinder <harvinder.chavan7@gmail.com>
2020-11-21 23:25:06 +0530

    Adde refs

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import cockPitClasses from "./Cockpit.module.css";
 
 const Cockpit = (props) => {
+  const btnRef = useRef(null);
   useEffect(() => {
     console.log("[Cockpit.js] initialized");
+    // btnRef.current.click();
   }, []); // Add arguments to array as dependencies on which to call the function
 
   const paraClasses = [];
@@ -20,7 +22,7 @@ const Cockpit = (props) => {
   return (
     <div className={cockPitClasses.Cockpit}>
       <p className={paraClasses.join(" ")}>List of persons</p>
-      <button className={btnClasses} onClick={props.clicked}>
+      <button ref={btnRef} className={btnClasses} onClick={props.clicked}>
         Toggle Persons
       </button>
     </div>
